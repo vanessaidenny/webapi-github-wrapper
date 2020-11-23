403a1af36ba6bb73c0ff3d7af20aa64c01056fa7
Vanessa Denny <vanessaidenny@hotmail.com>
2020-11-23 10:29:21 -0300

    refactor: Separate requests for test implemetation

--- WebApiWrapper/Services/ClientService.cs
+++ WebApiWrapper/Services/ClientService.cs
@@ -9,15 +9,19 @@ namespace webapi_github_wrapper.Services
 {
     public class ClientService : IClientService
     {
-        private static readonly HttpClient _client = new HttpClient();
-        
-        public async Task<List<Repository>> ClientRequest(string organizationName)
+        private readonly HttpClient _client;
+
+        public ClientService(HttpClient httpClient)
         {
+            _client = httpClient;
             _client.DefaultRequestHeaders.Accept.Clear();
             _client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             _client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+        }
 
+        public async Task<List<Repository>> ClientRequest(string organizationName)
+        {
             var login = $"https://api.github.com/orgs/{organizationName}/repos";
             var streamTask = _client.GetStreamAsync(login);
             var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
